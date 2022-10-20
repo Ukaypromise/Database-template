4fbe47755803ad4c13fec75a00da465ccea65e4d
Ukaypromise <promiseukaimo@gmail.com>
2022-10-20 05:57:23 -0400

    Add new tables

--- schema.sql
+++ schema.sql
@@ -9,3 +9,14 @@ CREATE TABLE animals (
     weight_kg DECIMAL,
     species VARCHAR(250)
 );
+
+CREATE TABLE owners (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(250),
+    age INT,    
+);
+
+CREATE TABLE species (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),   
+);
